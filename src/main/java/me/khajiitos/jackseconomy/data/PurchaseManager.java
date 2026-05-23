01faeb36554fbd752b36f7023ec43acf1f81a96f
SCsupercraft <103709272+SCsupercraft@users.noreply.github.com>
2026-05-23 18:43:25 +0100

    Fix purchase manager
    Somehow I made a mistake in 1.6.2 and isEmpty did the opposite of what it should of.

--- src/main/java/me/khajiitos/jackseconomy/data/PurchaseManager.java
+++ src/main/java/me/khajiitos/jackseconomy/data/PurchaseManager.java
@@ -208,7 +208,7 @@ public class PurchaseManager {
 		}
 
         public boolean isEmpty() {
-            return quantity == 0 || description.map(i -> i.item().value() != Items.AIR, f -> f.fluid().value() != Fluids.EMPTY);
+            return quantity == 0 || description.map(i -> i.item().value() == Items.AIR, f -> f.fluid().value() == Fluids.EMPTY);
         }
 	}
 
