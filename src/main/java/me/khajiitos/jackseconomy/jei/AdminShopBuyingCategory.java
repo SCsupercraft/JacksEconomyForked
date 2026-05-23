13869e7a80863d018f51420e85b7c2401e621f09
SCsupercraft <103709272+SCsupercraft@users.noreply.github.com>
2026-05-23 17:23:49 +0100

    Fix JEI integration

--- src/main/java/me/khajiitos/jackseconomy/jei/AdminShopBuyingCategory.java
+++ src/main/java/me/khajiitos/jackseconomy/jei/AdminShopBuyingCategory.java
@@ -78,7 +78,7 @@ public class AdminShopBuyingCategory implements IRecipeCategory<AdminShopBuyingC
 
 	@Override
 	public void setRecipe(@NotNull IRecipeLayoutBuilder builder, @NotNull Details details, @NotNull IFocusGroup focuses) {
-		if (details.description == null) return;
+		if (details.description == null || Objects.equals(details.price, BigDecimal.ZERO)) return;
 
 		ItemStack adminShopStack = new ItemStack(ItemBlockReg.ADMIN_SHOP);
 		CompoundTag tag = new CompoundTag();
