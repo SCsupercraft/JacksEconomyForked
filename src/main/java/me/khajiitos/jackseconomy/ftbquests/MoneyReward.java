04c8688aea8703359207574104b3e98fa7d04980
SCsupercraft <103709272+SCsupercraft@users.noreply.github.com>
2026-06-18 16:53:46 +0100

    Fix quest reward when no wallet is equipped

--- src/main/java/me/khajiitos/jackseconomy/ftbquests/MoneyReward.java
+++ src/main/java/me/khajiitos/jackseconomy/ftbquests/MoneyReward.java
@@ -10,6 +10,7 @@ import dev.ftb.mods.ftbquests.quest.reward.RewardType;
 import dev.ftb.mods.ftbquests.registry.ModItems;
 import me.khajiitos.jackseconomy.config.Config;
 import me.khajiitos.jackseconomy.curios.CuriosWallet;
+import me.khajiitos.jackseconomy.item.GoldenWalletItem;
 import me.khajiitos.jackseconomy.item.OIMWalletItem;
 import me.khajiitos.jackseconomy.item.WalletItem;
 import me.khajiitos.jackseconomy.packet.WalletBalanceDifPacket;
@@ -106,6 +107,12 @@ public class MoneyReward extends Reward {
                     ItemHelper.dropItem(left, player.level(), player.blockPosition());
                 }
             });
+        } else if (!(stack.getItem() instanceof GoldenWalletItem)) {
+            for (ItemStack item: CurrencyHelper.getCurrencyItems(BigDecimal.valueOf(amount))) {
+                if (!player.getInventory().add(item)) {
+                    ItemHelper.dropItem(item, player.level(), player.blockPosition());
+                }
+            }
         }
     }
 
